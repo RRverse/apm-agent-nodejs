8f4b11719d5ba97d37dbf44784a0ebb35f496dd9
Eneko Rodríguez <eneko.rodriguez@rrverse.com>
2024-06-06 12:04:32 +0200

    moved release for use

--- test/activation-method/fixtures/require1.js
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright Elasticsearch B.V. and other contributors where applicable.
- * Licensed under the BSD 2-Clause License; you may not use this file except in
- * compliance with the BSD 2-Clause License.
- */
-
-require('elastic-apm-node').start();
-console.log("using: require('elastic-apm-node').start()");
